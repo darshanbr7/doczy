da8b9a6286b2403800f8483b48d4fd60d4e7a571
Darshan B R <darshanbr770@gmail.com>
2025-02-17 17:46:08 +0530

    added home page

--- client/src/pages/mutual/Home.js
+++ client/src/pages/mutual/Home.js
@@ -1,12 +1,51 @@
-import { useSelector } from "react-redux"
-const Home = ( ) => {
-    const {userInfo }  = useSelector( state => state.auth );
-    
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import InfoPage from "../../images/InfoPage.jpg";
+
+const Home = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    };
+
     return (
-        <div>
-            <h2> Home Page </h2>
-        
+        <div className='flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-blue-100 via-blue-50 to-blue-100'>
+            <div className='flex justify-center mb-10'>
+                <h2 className="text-xl font-semibold overflow-hidden whitespace-nowrap border-r-4 border-solid pr-2 animate-typing ">
+                Trusted doctors, right at your fingertips. Fast and reliable medical care when you need it most.
+                </h2>
+            </div>
+
+            <div className='flex justify-center w-1/3 max-w-4xl'>
+                <div className="slider-container w-full">
+                    <Slider {...settings}>
+                        <div className="flex justify-center">
+                            <img
+                                src={InfoPage}
+                                alt="Slide 1"
+                                className="slider-image w-full h-auto rounded-sm"
+                            />
+                        </div>
+                        <div className="flex justify-center">
+                            <img
+                                src={InfoPage}
+                                alt="Slide 2"
+                                className="slider-image w-full h-auto rounded-sm"
+                            />
+                        </div>
+                    </Slider>
+                </div>
+            </div>
         </div>
-    )
-}
-export default Home
+    );
+};
+
+export default Home;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,11 +5,22 @@ module.exports = {
     extend: {
       animation: {
         spin: 'spin 1s linear infinite',
+        typing: 'typing 3s steps(40)  infinite',
+        blink: 'blink 0.75s step-end infinite',
       },
       keyframes: {
         spin: {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
+        },
+        typing: {
+          '0%': { width: '0' },
+          '100%': { width: '100%' },
+        },
+        blink: {
+          '0%': { borderColor: 'transparent' },
+          '50%': { borderColor: 'black' },
+          '100%': { borderColor: 'transparent' },
         },
       },
     },
