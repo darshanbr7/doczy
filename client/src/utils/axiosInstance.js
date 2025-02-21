3063edd37ca93151e68e902ff5dc87f128690af9
Darshan B R <darshanbr770@gmail.com>
2025-02-21 14:53:18 +0530

    deploying

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-    baseURL : "https://doczy-server.vercel.app/api"
+    baseURL : "https://doczyserver.vercel.app/api"
 })
 export default axiosInstance
